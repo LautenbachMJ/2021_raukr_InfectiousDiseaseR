db9c60a327becfab79e432205ab6bee608a5e211
jennolo <86223586+jennolo@users.noreply.github.com>
2021-06-22 10:48:55 +0200

    Function converting columns with dates from character to class Date

---
+++ src/date_from.R
@@ -0,0 +1,13 @@
+
+#' Converts columns with dates from character to class Date
+#'
+#' @param x A tibble with columns containing the string "date" that will get converted to class Date.
+#' @param formats the format in which the dates are, eg "%Y-%m-%d" etc.
+#'
+#' @return return same tibble with columns containing dates coverted to dates
+#' @export
+#'
+#' @examples
+date_form <- function(x, formats){
+  x %>% mutate(across(matches("date"),as.Date, formats))
+} 
