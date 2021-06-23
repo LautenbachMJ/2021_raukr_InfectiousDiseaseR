0669111674d5e1da5e11bb4fba39731d49cff3e8
Maximilian Julius Lautenbach <lautenbach.maximilian@googlemail.com>
2021-06-23 09:19:06 +0200

    read_delim creates tibble

--- src/data_workflow.R
+++ src/data_workflow.R
@@ -7,15 +7,15 @@ source("src/plot_param_per_day.R")
 
 
 #load input data
-input_data <- read.csv("src/input_data.csv", stringsAsFactors = FALSE, sep=";")
-input_data <- as.tibble(input_data)
+input_data <- read.csv("data/input_data.csv", stringsAsFactors = FALSE, sep=";")
+input_data <- as.tibble(input_data) ### Comment julius: I think, read_delim() (readr/tidyverse function) reads it automatically into a tibble
+
+#input_data <- read_delim("data/input_data.csv", delim = ";")
+
 
 #Convert columns with dates from Character to Date
 input_data <- date_from(input_data) #format same as default "%Y-%m-%d"
 
 #calculates number of days between dates and adds column with treatment status
 input_data_calc <- time_between(input_data,on_date = "onset_date", vis_date = "visit_date", treat_date = "treatment_date" )
-
-
-
 
