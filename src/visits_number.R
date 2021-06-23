a5d4ca61e4d88d1e9ab866c2a83a6bd8aded1d18
onadeulofeu <ona.deulofeu@gmail.com>
2021-06-23 10:52:16 +0200

    visits

--- src/visits_number.R
+++ src/visits_number.R
@@ -10,8 +10,8 @@
 #' @import tidyverse
 #'
 #' @examples
-visit_number <- function(input, Patient_ID, visit_date){
-  input %>% group_by(PatientID) %>% 
+visit_number <- function(input, patient_id, visit_date){
+  input %>% group_by(patient_id) %>% 
     arrange(visit_date) %>% 
     subset(param3 != "NA"  | param1 != "NA" | param2 != "NA" ) %>% 
     mutate(visit = 1:n())}
