e13d7d9d406dc63d8cbddde323f1082b44a0890a
Maximilian Julius Lautenbach <lautenbach.maximilian@googlemail.com>
2021-06-23 13:05:06 +0200

    sorry for rearranging workflow

--- src/data_workflow.R
+++ src/data_workflow.R
@@ -12,46 +12,44 @@ source("src/visits_number.R")
 input_data <- read_delim("data/input_data.csv", delim = ";")
 
 # extract parameter names
-<<<<<<< HEAD
 param_mapping <- param_parsing(input_data = input_data, input_param1 = "cd4", input_param2 = "cd8", input_param3 = "viral_load")
 time_mapping <- time_parsing(input_data = input_data, on_date = "onset_date", vis_date = "visit_date", treat_date = "treatment_date")
-=======
-param_mapping <- param_parsing(input_data = input_data,input_param1 = "cd4", input_param2 = "cd8", input_param3 = "viral_load")
 
-View(input_data)
->>>>>>> a3f35e7d95e5a94b2b69e682ce258522e0e361a8
 
-#Convert columns with dates from Character to Date
-input_data <- date_from(input_data) #format same as default "%Y-%m-%d"
-
-#calculates number of days between dates and adds column with treatment status
-date_cols <- time_mapping %>% names()
-
-<<<<<<< HEAD
-input_data %>% 
+## ============ 
+## data wrangling
+## ============ 
+#parsed_data <-
+  input_data %>% 
+  ## transform "_date" columns into date format
   date_from() %>% 
+  ## mapping input variable names to function "under-the-hood" variables
   rename(time_mapping) %>% 
-  rename(param_mapping) %>% 
-  time_between(date_cols) %>% 
+  rename(param_mapping) %>%
+  ## calculate days between time points
+  time_between(names(time_mapping)) %>% 
+  ## calculate numbers of visits
   visits_number()
-#input_data_calc <- time_between(input_data, on_date = "onset_date", vis_date = "visit_date", treat_date = "treatment_date" )
+  
 
-=======
->>>>>>> a3f35e7d95e5a94b2b69e682ce258522e0e361a8
+## ============ 
+## downstream 
+## ============ 
+parsed_data %>% 
+  ## plot cohort overview
+  ## requires param1, param2, param3 variables
+  overview_plot()
+  visits_number(param_mapping = param_mapping)
+
 ##visits number
 ## requires param1, param2, param3 variables
 input_data_calc_visit <- visits_number(input_data_calc)
 
-<<<<<<< HEAD
 ## plot cohort overview
 ## requires param1, param2, param3 variables
-=======
-##input_data_ed
-input_data_ed <- param_parsing(input_data) %>% time_between(on_date = "onset_date", vis_date = "visit_date", treat_date = "treatment_date") %>% visits_number()
-
-## plot
->>>>>>> a3f35e7d95e5a94b2b69e682ce258522e0e361a8
 overview_plot(parsed_data = data2, param_mapping = param_mapping)
+
+
 
 ## === Test of plot_param_per_day.R === ##
 
@@ -63,3 +61,8 @@ plot_param_per_day(data, y = viral_load) + labs(col="Patient ID") + xlab("Days p
 
 ## manual mapping
 #param_mapping <- c("param1" = "CD4","param2" = "CD8","param3" = "Viral_load")
+#Convert columns with dates from Character to Date
+input_data <- date_from(input_data) #format same as default "%Y-%m-%d"
+
+#calculates number of days between dates and adds column with treatment status
+date_cols <- time_mapping %>% names()
