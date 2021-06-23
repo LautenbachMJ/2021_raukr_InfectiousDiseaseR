55096052d9c7e11922cb5df9c32eb681bf0f7d81
jennolo <86223586+jennolo@users.noreply.github.com>
2021-06-23 08:55:18 +0200

    added default format

--- src/date_from.R
+++ src/date_from.R
@@ -2,13 +2,13 @@
 #' Converts columns with dates from character to class Date
 #'
 #' @param x A tibble with columns containing the string "date" that will get converted to class Date.
-#' @param formats the format in which the dates are, eg "%Y-%m-%d" etc.
+#' @param formats the format in which the dates are, default is "%Y-%m-%d"
 #'
 #' @return return same tibble with columns containing dates coverted to dates
 #' @export
 #'
 #' @examples
 #' date_form(df, "%Y-%m-%d")
-date_from <- function(x, formats){
+date_from <- function(x, formats="%Y-%m-%d"){
   x %>% mutate(across(matches("date"),as.Date, formats))
 } 
