66379e5b828a3a67bcf5e2fef9f39fc1a3273d0b
Maximilian Julius Lautenbach <lautenbach.maximilian@googlemail.com>
2021-06-23 10:36:41 +0200

    test fun into workflow

--- src/data_workflow.R
+++ src/data_workflow.R
@@ -14,4 +14,20 @@ input_data <- date_from(input_data) #format same as default "%Y-%m-%d"
 #calculates number of days between dates and adds column with treatment status
 input_data_calc <- time_between(input_data,on_date = "onset_date", vis_date = "visit_date", treat_date = "treatment_date" )
 
+## manual mapping
+param_mapping <- c("param1" = "CD4","param2" = "CD8","param3" = "Viral_load")
+
+## plot
+overview_plot(parsed_data = data2, param_mapping = param_mapping)
+
+
+
+## === Test of plot_param_per_day.R === ##
+
+data3 <- read_csv("data/output_file.csv")
+plot_param_per_day(data, y = cd4) + labs(col="Patient ID") + xlab("Days post onset") + ylab("CD4 levels")
+plot_param_per_day(data, y = cd8) + labs(col="Patient ID") + xlab("Days post onset") + ylab("CD8 levels")
+plot_param_per_day(data, y = viral_load) + labs(col="Patient ID") + xlab("Days post onset") + ylab("Viral load")
+
+
 
